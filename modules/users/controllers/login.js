74db6fe838f209f252c66d7e5cf0a2055a6d4fa3
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-16 03:40:41 +0600

    login route is working and sending response

--- modules/users/controllers/login.js
+++ modules/users/controllers/login.js
@@ -1,5 +1,15 @@
-const login = (req, res) =>{
+const mongoose = require("mongoose");
+const login = (req, res) => {
 
+    const usersModel = mongoose.model("users");
+
+
+    const { emain, password } = req.body;
+
+    res.status(200).json({
+        status: "Success",
+        message: "Login Successful"
+    })
 }
 
-module.exports= login;
+module.exports = login;
--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const usersModel = require("../../models/users.model");
 const register = require("./controllers/register");
+const login = require("./controllers/login");
 
 const userRoutes = express.Router();
 
 // routes here
 
 userRoutes.post("/register", register);
+userRoutes.post("/login", login);
 
 module.exports = userRoutes;
