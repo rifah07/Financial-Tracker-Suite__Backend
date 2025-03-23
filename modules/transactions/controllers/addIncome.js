5c2daf27a182bfcdbdc18f620f3d980232fa042d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-23 19:26:02 +0600

    Income amount validation implemented

--- modules/transactions/controllers/addIncome.js
+++ modules/transactions/controllers/addIncome.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const addIncome = (req, res) => {
   const usersModel = mongoose.model("users");
@@ -11,9 +12,12 @@ const addIncome = (req, res) => {
 
   if (remarks.length < 5) throw "REmarks must be at least 5 characters long.";
 
+  const checkAmountType = validator.isNumeric(amount.toString());
+  if (!checkAmountType) throw "Amount must be a number";
+
   res.status(200).json({
     status: "Success",
-    message: "Income added successfully"
+    message: "Income added successfully",
   });
 };
 
