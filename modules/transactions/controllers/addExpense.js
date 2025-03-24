3abce7065e1a92505a108dfc90e5338c02720f2b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-24 06:24:39 +0600

    Add Expense implemented and Added to route

---
+++ modules/transactions/controllers/addExpense.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const addExpense = async (req, res) => {
+  const usersModel = mongoose.model("users");
+  const transactionsModel = mongoose.model("transactions");
+
+  const { amount, remarks } = req.body;
+
+  if (!amount) throw "Amount is required";
+  if (!remarks) throw "Remarks is required";
+
+  if (remarks.length < 5) throw "REmarks must be at least 5 characters long.";
+
+  const checkAmountType = validator.isNumeric(amount.toString());
+  if (!checkAmountType) throw "Amount must be a number";
+
+  await transactionsModel.create({
+    user_id: req.user._id,
+    amount: amount,
+    transaction_type: "expense",
+    remarks: remarks,
+  });
+
+  await usersModel.updateOne(
+    {
+      _id: req.user._id,
+    },
+    {
+      $inc: {
+        balance: amount * -1,
+      },
+    },
+    {
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    status: "Success",
+    message: "Expense added successfully",
+  });
+};
+
+module.exports = addExpense;
--- modules/transactions/transactions.routes.js
+++ modules/transactions/transactions.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const auth = require("../../middleware/auth");
 const addIncome = require("./controllers/addIncome");
+const addExpense = require("./controllers/addExpense");
 
 const transactionRoutes = express.Router();
 
@@ -12,5 +13,6 @@ transactionRoutes.use(auth);
 //protected routes
 
 transactionRoutes.post("/addIncome", addIncome);
+transactionRoutes.post("/addExpense", addExpense);
 
 module.exports = transactionRoutes;
