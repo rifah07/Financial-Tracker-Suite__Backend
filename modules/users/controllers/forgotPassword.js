d4f6973e4e071419e45b1f3f0a56aaec5c852abc
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-03-27 01:56:09 +0600

    reset code sent to user

--- modules/users/controllers/forgotPassword.js
+++ modules/users/controllers/forgotPassword.js
@@ -1,24 +1,56 @@
 const mongoose = require("mongoose");
+const nodemailer= require("nodemailer");
 
-const forgotPassword = async (req,res) => {
+const forgotPassword = async (req, res) => {
+  const usersModel = mongoose.model("users");
 
-    const usersModel = mongoose.model("users");
+  const { email } = req.body;
 
-    const {email}= req.body;
+  if (!email) throw "Your E-mail must be provided";
 
-    if(!email) throw "Your E-mail must be provided";
+  const getUser = await usersModel.findOne({
+    email: email,
+  });
 
-    const getUser = await usersModel.findOne({
-        email: email
-    })
+  if (!getUser) throw "This email address does not exist in the system!";
 
-    if(!getUser) throw "This email address does not exist in the system!";
+  const resetCode = Math.floor(10000 + Math.random() * 90000); //this function creates 5 random digits in numbers
 
-    res.status(200).json({
-        status: "Reset code sent to email successfully"
-    })
+  await usersModel.updateOne(
+    {
+      email: email,
+    },
+    {
+      reset_code: resetCode,
+    },
+    {
+      runValidators: true,
+    }
+  );
 
 
-}
+  //send email
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "12ad86218f7fa0",
+        pass: "2af3ff3c8e638f",
+      },
+    });
+  
+    await transport.sendMail({
+      to: email,
+      from: "info@financialtrackersuite.com", // the domain of production server
+      text: "You have requested to reset password. Here is your reset code "+ resetCode,
+      html: "Your password reset code is "+ resetCode,
+      subject: "Password Reset Code for Financial Tracker Suite"
+    });
+  
+
+  res.status(200).json({
+    status: "Reset code sent to email successfully",
+  });
+};
 
 module.exports = forgotPassword;
--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -49,7 +49,7 @@ const register = async (req, res) => {
   await transport.sendMail({
     to: createdUser.email,
     from: "info@financialtrackersuite.com", // the domain of production server
-    text: "Welcome! You have successfully registered to Financial Tracker Pro. Thanks for choosing our platform. Hope you can track all your financial transactions here.",
+    text: "Welcome! You have successfully registered to Financial Tracker Suite. Thanks for choosing our platform. Hope you can track all your financial transactions here.",
     html: "<h1>Welcome! You have successfully registered to Financial Tracker Pro.</h1><br> <h5> Thanks for choosing us.</h5>",
     subject: "Greetings from Financial Tracker Suite."
   });
