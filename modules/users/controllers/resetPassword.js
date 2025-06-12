3d939abdc1ed50c059ff88f4eb2009b1ac11b94a
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-12 15:53:02 +0600

    reset password funtionality updated

--- modules/users/controllers/resetPassword.js
+++ modules/users/controllers/resetPassword.js
@@ -4,49 +4,32 @@ const emailManager = require("../../../managers/emailManager");
 
 const resetPassword = async (req, res) => {
   const usersModel = mongoose.model("users");
-
   const { email, new_password, reset_code } = req.body;
 
-  if (!email) throw "This email does not exist";
+  if (!email) throw "Email is required";
   if (!new_password) throw "Please enter a new password";
   if (!reset_code) throw "Reset code is required to update password";
-  if (new_password.length < 5)
-    throw "Password must be at least 5 characters long";
+  if (new_password.length < 5) throw "Password must be at least 5 characters long";
 
-  const getUserWithResetCode = await usersModel.findOne({
-    email: email,
-    reset_code: reset_code,
-  });
+  const user = await usersModel.findOne({ email, reset_code });
+  if (!user) throw "Reset code does not match or email is incorrect";
 
-  if (!getUserWithResetCode) throw "Reset code does not match";
-  const hashedPassword = await bcrypt.hash(new_password, 12);
-  getUserWithResetCode.reset_code = null;
-  await getUserWithResetCode.save();
+  // Update password
+  user.password = await bcrypt.hash(new_password, 12);
+  user.reset_code = null;
+  await user.save();
 
-  await usersModel.updateOne(
-    {
-      email: email,
-    },
-    {
-      password: hashedPassword,
-      //reset_code:""
-    },
-    {
-      runValidators: true,
-    }
-  );
-
-  //send email
+  // Send confirmation email
   await emailManager(
     email,
-    "Welcome! You have successfully reseted your passoword in Financial Tracker Suite. Thanks for being a valued member of our platform. Hope you can track all your financial transactions here.",
-    "Congratulations! You have successfully reseted your password in Financial Tracker Suite.<br> If you had not done that, please contact us",
-    "Password reset is successfull!"
+    "Your password has been reset successfully. You can now log in with your new password.",
+    "Congratulations! Your password has been reset in Financial Tracker Suite. <br>If this wasn't you, please contact support immediately.",
+    "Password Reset Successful"
   );
 
   res.status(200).json({
     status: "Success",
-    message: "Password updated successfully!!",
+    message: "Password updated successfully!",
   });
 };
 
