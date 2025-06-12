718d9b59fb58930199e485f280ec7f7cf17d6625
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-12 15:48:10 +0600

    forgot password functionality updated

--- modules/users/controllers/forgotPassword.js
+++ modules/users/controllers/forgotPassword.js
@@ -1,46 +1,36 @@
 const mongoose = require("mongoose");
 const emailManager = require("../../../managers/emailManager");
 
-const forgotPassword = async (req, res) => {
-  const usersModel = mongoose.model("users");
+const forgotPassword = async (req, res, next) => {
 
-  const { email } = req.body;
+  try {
+    const usersModel = mongoose.model("users");
+    const { email } = req.body;
 
-  if (!email) throw "Your E-mail must be provided";
+    if (!email) throw new Error("Your E-mail must be provided");
 
-  const getUser = await usersModel.findOne({
-    email: email,
-  });
+    const getUser = await usersModel.findOne({ email });
 
-  if (!getUser) throw "This email address does not exist in the system!";
+    if (!getUser) throw new Error("This email address does not exist in the system!");
 
-  const resetCode = Math.floor(10000 + Math.random() * 90000); //this function creates 5 random digits in numbers
+    const resetCode = Math.floor(10000 + Math.random() * 90000);
 
-  await usersModel.updateOne(
-    {
-      email: email,
-    },
-    {
-      reset_code: resetCode,
-    },
-    {
-      runValidators: true,
-    }
-  );
+    getUser.reset_code = resetCode;
+    await getUser.save(); // use save for better validation and hook compatibility
 
-  //send email
+    await emailManager(
+      email,
+      `You have requested to reset password. Here is your reset code ${resetCode}`,
+      `Your password reset code is ${resetCode}`,
+      "Password Reset Code for Financial Tracker Suite"
+    );
 
-  await emailManager(
-    email,
-    "You have requested to reset password. Here is your reset code " +
-      resetCode,
-    "Your password reset code is " + resetCode,
-    "Password Reset Code for Financial Tracker Suite"
-  );
-
-  res.status(200).json({
-    status: "Reset code sent to email successfully",
-  });
+    res.status(200).json({
+      status: "Reset code sent to email successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = forgotPassword;
